054ba073dbea065325e80c9bf57d167f122d7283
Matt Lunn <matthew.lunn@uk.tesco.com>
2020-03-08 09:15:19 +0000

    Fixing bathroom automation; continuous humidity updates meant the automation hardly ever ran

--- src/automations/bathroom.js
+++ src/automations/bathroom.js
@@ -2,37 +2,54 @@ import bus, { EVENT_START, EVENT_END } from '../bus';
 import { Device } from '../models';
 import { isWithinTime } from '../helpers/time';
 
-let timeoutToTurnOff;
+let timeoutToTurnOff = null;
+
+// Motion starts; immediately turn light on. Cancel any scheduled timeouts
+// Motion ends; schedule possible turnoff for light.
+// Humidity changes;
+//   If motion or timeout, ignore
+//   Otherwise if new value > max, turn light on
+//   Otherwise if new value < max, turn light off
 
 export default function ({ sensorName, lightName, maximumHumidity, offDelaySeconds = 0, between = [{ start: '00:00', end: '23:59', brightness: 100 }] }) {
   [EVENT_START, EVENT_END].forEach((eventEvent) => {
     bus.on(eventEvent, async (event) => {
       const sensor = await event.getDevice();
+      const light = await Device.findByName(lightName);
 
-      if (sensor.name === sensorName && (event.type === 'motion' || event.type === 'humidity')) {
-        const light = await Device.findByName(lightName);
-        const isLightOn = await light.getProperty('on');
+      if (sensor.name === sensorName) {
+        if (event.type === 'motion') {
+          if (eventEvent === EVENT_START) {
+            const isLightOn = await light.getProperty('on');
+            const { brightness = 100 } = between.find(({ start, end }) => isWithinTime(start, end)) || {};
 
-        clearTimeout(timeoutToTurnOff);
+            clearTimeout(timeoutToTurnOff);
+            timeoutToTurnOff = null;
 
-        // If someone has just walked in turn the light on (if it isn't on already)
-        if (event.type === 'motion' && eventEvent === EVENT_START) {
-          const { brightness = 100 } = between.find(({ start, end }) => isWithinTime(start, end)) || {};
+            if (!isLightOn) {
+              await light.setProperty('brightness', brightness);
+            }
+          } else if (eventEvent === EVENT_END) {
+            timeoutToTurnOff = setTimeout(async () => {
+              const humidity = await sensor.getProperty('humidity');
 
-          if (!isLightOn) {
-            await light.setProperty('brightness', brightness);
+              if (humidity < maximumHumidity) {
+                await light.setProperty('on', false);
+              }
+            }, offDelaySeconds * 1000);
           }
 
-        // Otherwise there is no more motion, or humidity has changed. In any of those
-        // cases, set a timeout to possibly turn the light off if the humidity is low enough.
-        } else {
-          timeoutToTurnOff = setTimeout(async () => {
-            const humidity = await sensor.getProperty('humidity');
+        } else if (event.type === 'humidity' && eventEvent === EVENT_START) {
+          const isSomeoneInRoom = await sensor.getProperty('motion');
 
-            if (humidity < maximumHumidity) {
-              await light.setProperty('on', false);
+          if (!isSomeoneInRoom && timeoutToTurnOff === null) {
+            const desiredLightState = event.value > maximumHumidity;
+            const actualLightState = await light.getProperty('on');
+
+            if (desiredLightState !== actualLightState) {
+              await light.setProperty('on', desiredLightState);
             }
-          }, offDelaySeconds * 1000);
+          }
         }
       }
     });
