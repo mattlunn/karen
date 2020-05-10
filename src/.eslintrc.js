a7a08848060076154d8ad2e9be14cbba1ba46a87
Matt Lunn <matthew.lunn@uk.tesco.com>
2020-05-10 12:26:03 +0100

    Adding eslint

---
+++ src/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  ignorePatterns: ['static/**/*'],
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
