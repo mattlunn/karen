df454e9abf9b93b74cd50eaac20a4366eafe845a
Matt Lunn <matt@mattlunn.me.uk>
2024-06-28 14:25:57 +0100

    Fixing webpack build

--- server/src/webpack.config.js
+++ server/src/webpack.config.js
@@ -19,6 +19,7 @@ module.exports = {
         loader: 'babel-loader',
         options: {
           presets: ['@babel/preset-react', ['@babel/preset-env', { targets: { browsers: ['last 2 Chrome versions'] }}]],
+          plugins: ['@babel/plugin-proposal-class-properties']
         }
       }]
     }, {
