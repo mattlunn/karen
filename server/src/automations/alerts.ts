fd94d7396f2cd243fb9657cf53496b2561a92ab6
Matt Lunn <matt@mattlunn.me.uk>
2024-12-12 21:03:55 +0000

    Updating threshold for system pressure

--- server/src/automations/alerts.ts
+++ server/src/automations/alerts.ts
@@ -3,7 +3,7 @@ import { createBackgroundTransaction } from '../helpers/newrelic';
 
 export default function () {
   bus.on(EVENT_START, createBackgroundTransaction(`automations:alerts`, async (event) => {
-    if (event.type === 'system_pressure' && event.value < 5) {
+    if (event.type === 'system_pressure' && event.value < 1) {
       bus.emit(NOTIFICATION_TO_ADMINS, {
         message: `🚨 Heating system pressure has fallen below 1 bar to ${event.value}.`
       });
