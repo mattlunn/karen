91da1f32b17b824b3caf9e56ef28856262f8a86f
Matt Lunn <matt@mattlunn.me.uk>
2025-05-16 16:59:07 +0100

    Durrr

--- server/src/automations/front-door.js
+++ server/src/automations/front-door.js
@@ -1,16 +1,19 @@
 import bus, { STAY_START, NOTIFICATION_TO_ADMINS } from '../bus';
 import { Device, Stay } from '../models';
+import logger from '../logger';
 import { createBackgroundTransaction } from '../helpers/newrelic';
 
 export default function ({ doorLockName }) {
   bus.on(STAY_START, createBackgroundTransaction('automations:front-door:stay-start', async (stay) => {
+    logger.info('RUNNING ' + stay.arrivalTrigger);
+
     if (stay.arrivalTrigger === 'geolocation') {
       const [
         device,
         user
-      ] = Promise.all([
-        await Device.findByName(doorLockName),
-        await stay.getUser(),
+      ] = await Promise.all([
+        Device.findByName(doorLockName),
+        stay.getUser(),
       ]);
 
       await device.getLockCapability().setIsLocked(false);
