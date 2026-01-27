a8bc9ead54ff81878a4b028cd8b065eef1e055aa
Matt Lunn <matt@mattlunn.me.uk>
2026-01-27 12:33:47 +0500

    Changing battery low indicator

--- server/src/components/device-control.js
+++ server/src/components/device-control.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ThermostatHeatMap from './thermostat-heat-map';
 import classNames from 'classnames';
-import { faSync } from '@fortawesome/free-solid-svg-icons';
-import { Indicator } from '@mantine/core';
+import { faBatteryEmpty, faSync } from '@fortawesome/free-solid-svg-icons';
 
 function getIsBatteryLow(device) {
   const batteryLowCapability = device.capabilities.find(x => x.type === 'BATTERY_LOW_INDICATOR');
@@ -21,15 +20,19 @@ export default function DeviceControl({ icon, iconOnClick = (e) => e.preventDefa
   return (
     <>
       <div className="device-control__header">
-        <Indicator color="red" disabled={!isBatteryLow} size={12} offset={4}>
-          <a className={classNames('device-control__icon-container', actionPending && 'device-control__icon-container--disabled')} style={{ backgroundColor: colorIconBackground ? color + '50' : 'transparent' }} onClick={iconOnClick} href="#">
-            <FontAwesomeIcon icon={actionPending ? faSync : icon} spin={actionPending} color={color} />
-          </a>
-        </Indicator>
+        <a className={classNames('device-control__icon-container', actionPending && 'device-control__icon-container--disabled')} style={{ backgroundColor: colorIconBackground ? color + '50' : 'transparent' }} onClick={iconOnClick} href="#">
+          <FontAwesomeIcon icon={actionPending ? faSync : icon} spin={actionPending} color={color} />
+        </a>
         <div className="device-control__body">
           <h4 className="device-control__name">{device.name}</h4>
           <ul className="device-control__values">
             {values.map((value, idx) => <li className="device-control__value" key={idx}>{value}</li>)}
+            
+            {isBatteryLow && (
+              <li className="device-control__value device-control__value--warning">
+                <FontAwesomeIcon icon={faBatteryEmpty} color="red" />
+              </li>
+            )}
           </ul>
         </div>
       </div>
