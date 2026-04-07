d574835cc3d56cefcdc555aaa8f8648f40667aa9
Matt Lunn <matt@mattlunn.me.uk>
2026-04-07 19:48:01 +0100

    Fixing issue with Unifi Login

--- server/src/services/unifi/lib/index.js
+++ server/src/services/unifi/lib/index.js
@@ -5,28 +5,18 @@ import logger from '../../../logger';
 const controller = new Controller({
   host: config.unifi.host,
   port: config.unifi.port,
-  site: config.unifi.site,
-  sslverify: false
+  username: config.unifi.username,
+  password: config.unifi.password,
 });
 
-async function ensureSession(name, fn) {
-  try {
-    return await fn();
-  } catch (err) {
-    if (err.message === 'api.err.LoginRequired') {
-      logger.error(`UniFi ${name} call failed because authentication required. Logging in and retrying...`);
-      await controller.login(config.unifi.username, config.unifi.password);
-      return await fn();
-    }
-    logger.error(`UniFi ${name} call failed`, err);
-    throw err;
-  }
+export async function getAllUsers() {
+  await controller.login();
+
+  return await controller.getAllUsers();
 }
 
-export function getAllUsers() {
-  return ensureSession('getAllUsers', () => controller.getAllUsers());
-}
+export async function getClientDevices() {
+  await controller.login();
 
-export function getClientDevices() {
-  return ensureSession('getClientDevices', () => controller.getClientDevices());
+  return await controller.getClientDevices();
 }
